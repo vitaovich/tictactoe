854e591029e11aa1ecdfbfa05d904c928ac2b999
vitaovich <vitaliyalekhnovich@gmail.com>
2023-07-30 19:10:13 +0000

    refactor button to seperate component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import React from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
+
+const Square: React.FC<{ value:string }> = (props) => {
+  return <button className="flex items-center justify-center w-8 h-8 m-2 rounded-md border-2 border-indigo-600 bg-indigo-200">{props.value}</button>
+}
 
 export default function Home() {
   return (
@@ -12,19 +17,19 @@ export default function Home() {
         <h1>Tic Tac Toe</h1>
         <div className="flex flex-col">
           <div className="flex flex-row">
-            <button className="flex items-center justify-center w-8 h-8 m-2 rounded-md border-2 border-indigo-600 bg-indigo-200">1</button>
-            <button className="flex items-center justify-center w-8 h-8 m-2 rounded-md border-2 border-indigo-600 bg-indigo-200">2</button>
-            <button className="flex items-center justify-center w-8 h-8 m-2 rounded-md border-2 border-indigo-600 bg-indigo-200">3</button>
+            <Square value='1'/>
+            <Square value='1'/>
+            <Square value='1'/>
           </div>
           <div className="flex flex-row">
-            <button className="flex items-center justify-center w-8 h-8 m-2 rounded-md border-2 border-indigo-600 bg-indigo-200">4</button>
-            <button className="flex items-center justify-center w-8 h-8 m-2 rounded-md border-2 border-indigo-600 bg-indigo-200">5</button>
-            <button className="flex items-center justify-center w-8 h-8 m-2 rounded-md border-2 border-indigo-600 bg-indigo-200">6</button>
+            <Square value='1'/>
+            <Square value='1'/>
+            <Square value='1'/>
           </div>
           <div className="flex flex-row">
-            <button className="flex items-center justify-center w-8 h-8 m-2 rounded-md border-2 border-indigo-600 bg-indigo-200">7</button>
-            <button className="flex items-center justify-center w-8 h-8 m-2 rounded-md border-2 border-indigo-600 bg-indigo-200">8</button>
-            <button className="flex items-center justify-center w-8 h-8 m-2 rounded-md border-2 border-indigo-600 bg-indigo-200">9</button>
+            <Square value='1'/>
+            <Square value='1'/>
+            <Square value='1'/>
           </div>
         </div>
       </div>
