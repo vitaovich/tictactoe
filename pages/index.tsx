ea9072785de9529195965ed4e2d5f5de56fd1a36
vitaovich <vitaliyalekhnovich@gmail.com>
2023-07-30 20:03:46 +0000

    implement history to jump to past turns

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,19 +18,45 @@ export default function Home() {
 }
 
 const Game: React.FC = () => {
-  const [xIsNext, setXIsNext] = useState<boolean>(true);
-  const [history, setHistory] = useState<[string[]]>([Array(9).fill('')]);
-  const currentSquares = history[history.length - 1];
+  const [history, setHistory] = useState<string[][]>([Array(9).fill('')]);
+  const [currentMove, setCurrentMove] = useState<number>(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares: string[]) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
+
+  function jumpTo(nextMove:number)
+  {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description: string;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <>
       <h1>Tic Tac Toe</h1>
       <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      <div className="border-2">
+        <ol>
+          {moves}
+        </ol>
+      </div>
     </>
   );
 }
