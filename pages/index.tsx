17a26b09c4862f969e3e03db5cc1dd41b53700c0
vitaovich <vitaliyalekhnovich@gmail.com>
2023-07-30 19:15:41 +0000

    Implement state for clicking on square to add X

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,26 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import React from 'react'
+import { useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
-const Square: React.FC<{ value:string }> = (props) => {
-  return <button className="flex items-center justify-center w-8 h-8 m-2 rounded-md border-2 border-indigo-600 bg-indigo-200">{props.value}</button>
+const Square: React.FC = () => {
+  const [value, setValue] = useState<string>('');
+
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    <button 
+      className="flex items-center justify-center w-8 h-8 m-2 rounded-md border-2 border-indigo-600 bg-indigo-200"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+
+  );
 }
 
 export default function Home() {
@@ -17,19 +32,19 @@ export default function Home() {
         <h1>Tic Tac Toe</h1>
         <div className="flex flex-col">
           <div className="flex flex-row">
-            <Square value='1'/>
-            <Square value='1'/>
-            <Square value='1'/>
+            <Square />
+            <Square />
+            <Square />
           </div>
           <div className="flex flex-row">
-            <Square value='1'/>
-            <Square value='1'/>
-            <Square value='1'/>
+            <Square />
+            <Square />
+            <Square />
           </div>
           <div className="flex flex-row">
-            <Square value='1'/>
-            <Square value='1'/>
-            <Square value='1'/>
+            <Square />
+            <Square />
+            <Square />
           </div>
         </div>
       </div>
