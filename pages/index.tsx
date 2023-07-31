ca9444a389a1afcde6e6bb6be10f1d146b3533aa
vitaovich <vitaliyalekhnovich@gmail.com>
2023-07-31 03:53:05 +0000

    highlight winning squares and check if game is a draw

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,6 +23,7 @@ const Game: React.FC = () => {
   const [currentMove, setCurrentMove] = useState<number>(0);
   const [moveOrder, setMoveOrder] = useState<boolean>(true);
   const xIsNext = currentMove % 2 === 0;
+  const isGameOver = history.length > 9;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares: string[]) {
@@ -66,7 +67,7 @@ const Game: React.FC = () => {
 
   return (
     <>
-      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} isGameOver={isGameOver} />
       <div className="flex flex-row space-x-4 bg-white rounded-md border border-gray-500 p-4">
         <div>
           <button onClick={reverseMoveOrder} className="border border-green-500 bg-green-200 rounded-md px-2 py-1 mb-2">Sort</button>
@@ -79,8 +80,8 @@ const Game: React.FC = () => {
   );
 }
 
-const Board: React.FC<{ xIsNext: boolean, squares: string[], onPlay: (nextSquares: string[]) => void }> = (props) => {
-  const winner = calculateWinner(props.squares);
+const Board: React.FC<{ xIsNext: boolean, squares: string[], onPlay: (nextSquares: string[]) => void, isGameOver: boolean }> = (props) => {
+  const winnerSquares = calculateWinner(props.squares);
   const boardSize = 3;
   let status: string;
 
@@ -96,9 +97,15 @@ const Board: React.FC<{ xIsNext: boolean, squares: string[], onPlay: (nextSquare
     props.onPlay(nextSquares);
   }
 
-  if (winner) {
+  if (winnerSquares) {
+    const winner = props.squares[winnerSquares[0]]
     status = "Winner: " + winner;
-  } else {
+  } 
+  else if (props.isGameOver)
+  {
+    status = "Draw"
+  }
+  else {
     status = "Next player: " + (props.xIsNext ? "X" : "O");
   }
 
@@ -107,7 +114,8 @@ const Board: React.FC<{ xIsNext: boolean, squares: string[], onPlay: (nextSquare
     const boardRow = [];
     for (let j = 0; j < boardSize; j++) {
       let location = (i * boardSize) + j;
-      boardRow.push(<Square key={location} value={props.squares[location]} onSquareClick={() => handleClick(location)} />);
+      const isWinner = winnerSquares?.includes(location);
+      boardRow.push(<Square key={location} value={props.squares[location]} onSquareClick={() => handleClick(location)} isWinner={isWinner} />);
     }
     boardRows.push(<div key={i} className='flex flex-row'>{boardRow}</div>);
   }
@@ -122,10 +130,12 @@ const Board: React.FC<{ xIsNext: boolean, squares: string[], onPlay: (nextSquare
   );
 }
 
-const Square: React.FC<{ value: string, onSquareClick: () => void }> = (props) => {
+const Square: React.FC<{ value: string, onSquareClick: () => void, isWinner: boolean | undefined }> = (props) => {
+  const buttonClasses = props.isWinner ? 'border-green-600 bg-green-200' : 'border-indigo-600 bg-indigo-200';
+  
   return (
     <button
-      className="flex items-center justify-center w-12 h-12 m-1 rounded-md border-2 border-indigo-600 bg-indigo-200"
+      className={`flex items-center justify-center w-12 h-12 m-1 rounded-md border-2 ${buttonClasses}`}
       onClick={props.onSquareClick}
     >
       {props.value}
@@ -148,7 +158,7 @@ function calculateWinner(squares: string[]) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return lines[i];
     }
   }
   return null;
